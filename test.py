cf9223b418490e7b160fc053008aed030fae1b60
brandon-does-code <brandon-does-code@users.noreply.github.com>
2021-01-20 19:42:24 +1300

    Add shit

---
+++ test.py
@@ -0,0 +1,12 @@
+import websocket, json, pprint, talib, numpy
+import config  # Contains API keys
+from binance.client import Client  # Primary binance API calls
+from binance.enums import *  # Imports global constants used in binance
+
+client = Client(config.API_KEY, config.API_SECRET)
+
+current_balance = float(
+                [i for i in client.get_account()['balances'] if
+                 i['asset'] == 'ETH'][0]['free'])
+            optimum_buy = float(current_balance) / float(close)
+            SELL_QUANTITY = round(optimum_buy, 5) - 0.001
