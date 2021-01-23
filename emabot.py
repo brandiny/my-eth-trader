0c416abefdcffa1d10d9b7100a3c2c5a8b87b28f
brandon-does-code <brandon-does-code@users.noreply.github.com>
2021-01-24 00:12:55 +1300

    MACD RSI SMA proper

--- emabot.py
+++ emabot.py
@@ -161,31 +161,38 @@ def on_message(ws, message):
             macd, macdsignal, macdhist = talib.MACD(np_closes, fastperiod=MACD_FASTPERIOD, slowperiod=MACD_SLOWPERIOD, signalperiod=MACD_SIGNALPERIOD)
             dump_data['macd'] = list(macd[:])
 
+            # Determine crosses
+            RSI_BULL_CROSS  = rsi[-1] > 50 and rsi[-1] > rsi[-2]
+            RSI_BEAR_CROSS  = rsi[-1] < 50 and rsi[-1] < rsi[-2]
+            MACD_BULL_CROSS = (macdsignal < 0 and macd < 0) and (macd[-1] > macdsignal[-1] and macd[-2] < macdsignal[-2])
+            MACD_BEAR_CROSS = (macdsignal > 0 and macd > 0) and (macd[-1] < macdsignal[-1] and macd[-2] > macdsignal[-2])
+            SMA_BULL        = sma_10[-1] > close
+            SMA_BEAR        = sma_10[-1] < close
+
             # LOG INFORMATION FOR LIVE DEBUG
-            print('rsi is overbought: ', rsi[-1] > RSI_OVERBOUGHT)
-            print('rsi is oversold: ', rsi[-1] < RSI_OVERSOLD)
-            print('in position: ', in_position)
-            print('buy quanitity: ', BUY_QUANTITY)
-            print('sell quanitity: ', SELL_QUANTITY)
-            print('stop loss: ', STOP_LOSS)
-            print('sma: ', sma_10[-1])
-            print('macd: ', macd[-1])
-            print('macd signal: ', macdsignal[-1])
-            '''
-            print('ema {}: '.format(str(EMA_LONG_PERIOD)), ema_long)
-            print('ema {}: '.format(str(EMA_SHORT_PERIOD)), ema_short)
-            print('bull cross occurring: ', ((ema_short[-2] < ema_long[-2]) and (ema_short[-1] > ema_long[-1])))
-            print('bear cross occurring: ', ((ema_short[-2] > ema_long[-2]) and (ema_short[-1] < ema_long[-1])))
-            '''
+            print('In position: ', in_position)
+            print('Buy quanitity: ', BUY_QUANTITY)
+            print('Sell quanitity: ', SELL_QUANTITY)
+            print('Stop loss: ', STOP_LOSS)
+            print('SMA: ', sma_10[-1])
+            print('MACD: ', macd[-1])
+            print('MACD signal: ', macdsignal[-1])
+            print('RSI BULL CROSS: ', RSI_BULL_CROSS)
+            print('RSI BEAR CROSS: ', RSI_BEAR_CROSS)
+            print('MACD BULL CROSS: ', MACD_BULL_CROSS)
+            print('MACD BEAR CROSS: ', MACD_BEAR_CROSS)
+            print('SMA BULL: ', SMA_BULL)
+            print('SMA BEAR: ', SMA_BEAR)
+
             # SELL CONDITION
             # For this strategy:
             #       - sell if in position and the momentum is turning
             #       - or if the trade price dips below the stop loss
-            if rsi[-1] < 50 and rsi[-1] < rsi[-2] and close < sma_10[-1] and macd[-1] - macdsignal[-1] < 0:
+            if RSI_BEAR_CROSS and MACD_BEAR_CROSS and SMA_BEAR:
                 if not in_position:
                     print('SELL SIGNAL. We have nothing to sell however. Nothing is done.')
                 else:
-                    print("SELL SIGNAL. EMA 8 has dipped below the EMA 21 indicating incoming bear market.")
+                    print("SELL SIGNAL. Exiting position")
                     order_succeeded = order(SIDE_SELL, SELL_QUANTITY, TRADE_SYMBOL)
                     if order_succeeded:
                         in_position = False
@@ -194,7 +201,7 @@ def on_message(ws, message):
             # For this strategy:
             #       - buy if the market has JUST exited a squeeze
             #       - has upwards momentum
-            if rsi[-1] > 50 and rsi[-1] > rsi[-2] and close > sma_10[-1] and macd[-1] - macdsignal[-1] > 0:
+            if MACD_BULL_CROSS and RSI_BULL_CROSS and SMA_BULL:
                 if in_position:
                     print("BUY SIGNAL. Already holding currency, so nothing is done.")
                 else:
